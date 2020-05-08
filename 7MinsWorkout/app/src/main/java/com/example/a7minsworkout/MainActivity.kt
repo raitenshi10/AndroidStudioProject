cbf314239782ae12e933a19d13bc4d83b35bf4e0
ryunoesuke <ilhampti3@gmail.com>
2020-05-08 10:29:42 +0700

    seeting manifest

--- 7MinsWorkout/app/src/main/java/com/example/a7minsworkout/MainActivity.kt
+++ 7MinsWorkout/app/src/main/java/com/example/a7minsworkout/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.a7minsworkout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        ll_start_button.setOnClickListener {
+           startActivity(Intent(this, ExerciseActivity::class.java ))
+        }
     }
 }
