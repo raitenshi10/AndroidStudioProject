b8d39090c11b063d2a2e0fe6269fc4f6eaef2a5c
ryunoesuke <ilhampti3@gmail.com>
2020-05-09 21:29:54 +0700

    adding the circula timer view

--- 7MinsWorkout/app/src/main/java/com/example/a7minsworkout/ExerciseActivity.kt
+++ 7MinsWorkout/app/src/main/java/com/example/a7minsworkout/ExerciseActivity.kt
@@ -2,11 +2,21 @@ package com.example.a7minsworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_exercise.*
 
 class ExerciseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercise)
+
+        setSupportActionBar(toolbar_exercise)
+        val actionbar = supportActionBar
+        if (actionbar != null) {
+            actionbar.setDisplayHomeAsUpEnabled(true)
+        }
+        toolbar_exercise.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
