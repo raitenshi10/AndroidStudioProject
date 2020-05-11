746d3f4f61c591b06486b9002189779afdf5f653
ryunoesuke <ilhampti3@gmail.com>
2020-05-11 08:15:41 +0700

    initial commit

--- TestCountDownTimer/app/src/main/java/com/example/testcountdowntimer/MainActivity.kt
+++ TestCountDownTimer/app/src/main/java/com/example/testcountdowntimer/MainActivity.kt
@@ -9,9 +9,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     // Variable waktu hitung mundur
-    private lateinit var countDownTimer: CountDownTimer
+    private  var countDownTimer: CountDownTimer? = null
     // Variable durasi waktu hitung mundur dalam milisecond ( second x 1000 )
     private var timeDuration: Long = 60000
+    private var pauseOffset: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,35 +21,45 @@ class MainActivity : AppCompatActivity() {
 
         tv_timer.text = "${(timeDuration/1000).toString()}"
         btn_start.setOnClickListener {
-            onStartTime()
+            onStartTime(pauseOffset)
         }
 
         btn_reset.setOnClickListener {
             onResetTime()
         }
+
+        btn_pause.setOnClickListener {
+            onPauseTime()
+        }
     }
 
-    fun onStartTime() {
-        countDownTimer = object : CountDownTimer(60000, 1000){
+    private fun onStartTime(pauseOffsetL: Long?) {
+        countDownTimer = object : CountDownTimer(timeDuration-pauseOffset, 1000){
             override fun onFinish() {
                 Toast.makeText(this@MainActivity, "Finished", Toast.LENGTH_SHORT)
                     .show()
             }
 
             override fun onTick(millisUntilFinished: Long) {
+                pauseOffset = timeDuration - millisUntilFinished
                 tv_timer.text = (millisUntilFinished/1000).toString()
             }
         }.start()
     }
 
-    fun onResetTime() {
+    private fun onResetTime() {
         if (countDownTimer != null) {
-            countDownTimer.cancel()
+            countDownTimer!!.cancel()
             tv_timer.text = (timeDuration/1000).toString()
+            countDownTimer = null
+            pauseOffset = 0
         }
     }
 
-    fun onPauseTime(){
+    private fun onPauseTime(){
+        if (countDownTimer != null) {
+            countDownTimer!!.cancel()
+        }
 
     }
 }
